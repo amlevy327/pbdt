6169f779768cdddbaf36a067d9e3e5cd0db12bd9
Andrew Levy <amlevy327@gmail.com>
2019-04-01 18:27:08 -0700

    added user nutrition, foods to user serializer

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,7 +1,8 @@
 class V1::UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:update]
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(new_user_params)
     
     if @user.save
       render json: @user, status: :ok
@@ -10,9 +11,26 @@ class V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    current_user.update(update_user_params)
+
+    if current_user.save
+      render json: current_user, status: :ok
+    else
+      render json: current_user.errors, status: :unprocessable_identity
+    end
+    
+  end
+
   private
 
-  def user_params
+  def new_user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end 
+
+  def update_user_params
+    params.require(:user).permit(:beans_g, :berries_g, :other_fruits_g, :cruciferous_vegetables_g,
+      :greens_g, :other_vegetables_g, :flaxseeds_g, :nuts_g, :turmeric_g, :whole_grains_g, :other_seeds_g,
+      :cals_g, :fat_g, :carbs_g, :protein_g)
   end
 end
--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -1,7 +1,8 @@
 class FoodSerializer < ActiveModel::Serializer
   attributes [
-    :id,
-    :name, 
-    :variety
+    :id, :name, :variety, :ss_amt_wt_t, :ss_unit_wt_t, :ss_amt_vol_t, :ss_unit_vol_t,
+    :beans_t, :berries_t, :other_fruits_t, :cruciferous_vegetables_t, :greens_t,
+    :other_vegetables_t, :flaxseeds_t, :nuts_t, :turmeric_t, :whole_grains_t, :other_seeds_t,
+    :cals_t, :fat_t, :carbs_t, :protein_t
   ]
 end
