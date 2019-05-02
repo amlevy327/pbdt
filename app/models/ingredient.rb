62661f9443c18607b18199632e5f224b50073bd4
Andrew Levy <amlevy327@gmail.com>
2019-05-01 19:21:54 -0700

    various-mods

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
+
+  delegate :user, :to => :recipe, :allow_nil => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :foods, dependent: :destroy
 
+  has_many :ingredients, through: :recipes
+
   acts_as_token_authenticatable
 
   # Include default devise modules. Others available are:
